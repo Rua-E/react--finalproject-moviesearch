2459995acf9633b236be49af0e94353868927744
Rua <rua.elkasheef@gmail.com>
2024-10-01 01:47:33 -0400

    fixing for depolyment

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <Router>
         <Routes>
           <Route path="/" element={<Home/>}></Route>
-          <Route path="/Movies" element={<MovieSearch/>}>Fixing</Route>
+          <Route path="/Movies" element={<MovieSearch/>}></Route>
           <Route path="/:Id" element={<MovieResults/>}></Route>
         </Routes>
      </Router>
